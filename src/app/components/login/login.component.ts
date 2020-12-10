2e4487c37db78d9dd63b275eea75c4b65c54070f
vishwasprabhu98 <vishwasprabhu8@gmail.com>
2020-12-10 08:53:46 +0530

    added validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,6 +16,7 @@ export class LoginComponent implements OnInit {
   constructor(private authService: AuthService) {
     this.error = false;
     this.disabledButton = true;
+    this.password = '';
   }
 
   ngOnInit(): void {
--- src/app/components/modal/add-admin/add-admin.component.ts
+++ src/app/components/modal/add-admin/add-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AdminService } from 'src/app/services/admin.service';
+import { ValidationService } from 'src/app/services/validation.service';
 
 @Component({
   selector: 'app-add-admin',
@@ -13,15 +14,21 @@ export class AddAdminComponent implements OnInit {
   @Input() adminCategory;
   @Output() resetAdmin = new EventEmitter();
 
-  constructor(private admin: AdminService) { }
+  constructor(private admin: AdminService,
+              private validate: ValidationService) { }
 
   ngOnInit(): void {
   }
 
   addNewAdminToAdminList(): void {
-    this.newAdmin.id = Math.floor(Math.random() * 10000 + 1);
-    this.newAdmin.type = this.adminCategory;
-    this.admin.addNewAdminToAdminList({ ...this.newAdmin });
-    this.resetAdmin.emit();
+    if (this.validate.validateEmail(this.newAdmin.email) &&
+        this.validate.validateEmployeeCode(this.newAdmin.employeeNumber) &&
+        this.validate.validateUsername(this.newAdmin.name)
+    ){
+      this.newAdmin.id = Math.floor(Math.random() * 10000 + 1);
+      this.newAdmin.type = this.adminCategory;
+      this.admin.addNewAdminToAdminList({ ...this.newAdmin });
+      this.resetAdmin.emit();
+    }
   }
 }
